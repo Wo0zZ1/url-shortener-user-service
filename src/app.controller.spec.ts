ed76cdacd3a60756312b869495b4d1f62f42a138
Wo0zZ1 <Mper06@mail.ru>
2025-11-02 21:35:27 +0300

    test: mock Prisma service in healthCheck tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,19 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
-import { PrismaModule } from './prisma/prisma.module'
-
 import { AppController } from './app.controller'
 
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 describe('AppController', () => {
 	let appController: AppController
 
 	beforeEach(async () => {
 		const app: TestingModule = await Test.createTestingModule({
-			imports: [PrismaModule],
 			controllers: [AppController],
-			providers: [AppService],
+			providers: [
+				AppService,
+				{
+					provide: PrismaService,
+					useValue: {
+						$queryRaw: jest.fn().mockResolvedValue([1]),
+					},
+				},
+			],
 		}).compile()
 
 		appController = app.get<AppController>(AppController)
